2ac211b88cd77d92bc0bb0f7cc79995385eb3e5e
Hershal0-0 <hershalrao@gmail.com>
2021-07-25 21:32:10 +0530

    new changes to Calculator App

--- CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -10,6 +10,9 @@ class MainActivity : AppCompatActivity() {
 //    To activate ViewBinding
     private lateinit var binding: ActivityMainBinding
 
+    var lastNumeric : Boolean =false
+    var lastDot : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //    To activate ViewBinding
@@ -18,8 +21,36 @@ class MainActivity : AppCompatActivity() {
     }
     fun onDigit(view:View){
         binding.tvInput.append((view as Button).text)
+        lastNumeric=true
     }
     fun onClear(view : View){
         binding.tvInput.setText("")
+        lastNumeric=false
+        lastDot=false
     }
+
+    fun onDecimalPoint(view : View){
+        if (lastNumeric && !lastDot){
+            binding.tvInput.append(".")
+            lastNumeric=false
+            lastDot=true
+        }
+    }
+
+    fun onOperator(view : View){
+         if(lastNumeric && !isOperatorAdded(binding.tvInput.text.toString())){
+             binding.tvInput.append((view as Button).text)
+             lastNumeric=false
+             lastDot=false
+         }
+    }
+
+    private fun isOperatorAdded(value : String) : Boolean{
+        return if (value.startsWith("-")){
+            false
+        }else{
+            value.contains("+") || value.contains("-") || value.contains("*") || value.contains("/")
+        }
+    }
+
 }
